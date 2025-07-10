9f9eac497c168966893510e592bf86fb95b13ce7
Axer- <optic.visuals01@gmail.com>
2025-07-09 21:08:32 -0400

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,7 @@
+document.getElementById('youtubeButton').onclick = function() {
+    window.location.href = "https://www.youtube.com/AxerDesign"; // Replace with your actual YouTube link
+};
+
+document.getElementById('scratchButton').onclick = function() {
+    window.location.href = "https://scratch.mit.edu/users/Axer-/"; // Replace with your actual Scratch link
+};
